ce150b2aaf39c54787cfd6b0358dfe54d01cb113
Amstel <dennis.brough@yandex.com>
2022-11-10 12:59:43 +0100

    test for pagination via skip and limit modifiers

--- test/read_test.js
+++ test/read_test.js
@@ -2,11 +2,26 @@ const assert = require("assert");
 const Friend = require("../src/friendSchema");
 
 describe("Test to read a friend from a db", () => {
-  let joe;
+  let joe, alice, kunmi, jane, zeb, clye;
 
   beforeEach((done) => {
     joe = new Friend({ name: "Joe" });
-    joe.save().then(() => done());
+    alice = new Friend({ name: "Alice" });
+    kunmi = new Friend({ name: "Kunmi" });
+    jane = new Friend({ name: "Jane" });
+    zeb = new Friend({ name: "Zeb" });
+    clye = new Friend({ name: "Clye" });
+
+    Promise.all([
+      joe.save(),
+      alice.save(),
+      kunmi.save(),
+      jane.save(),
+      zeb.save(),
+      clye.save(),
+    ]).then(() => {
+      done();
+    });
   });
 
   it("find all friends with a name of joe", (done) => {
@@ -25,5 +40,21 @@ describe("Test to read a friend from a db", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip and limit the result set", (done) => {
+    // when the query is made, skip first 2 result and limit the result got to 2 docs
+    Friend.find({})
+      .sort({ name: 1 })
+      .skip(2)
+      .limit(2)
+      .then((friends) => {
+        // console.log(friends[0].name);
+        // console.log(friends[1].name);
+        assert(friends.length === 2);
+        assert(friends[0].name === "Jane");
+        assert(friends[1].name === "Joe");
+        done();
+      });
   });
 });
